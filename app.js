21a509857321029f45965d9041149a05f744ec1c
Tzuyi Yang <a0988426059@gmail.com>
2019-06-05 23:11:01 +0800

    add front and back entrance and setting database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(3000, () => {
+  db.sequelize.sync()
   console.log('Example app listening on http://localhost:3000')
 })
+
+require('./routes')(app)
