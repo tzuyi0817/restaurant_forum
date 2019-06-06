963aaf6f6cec65e9f280db1dd953ef7a6ec244cf
Tzuyi Yang <a0988426059@gmail.com>
2019-06-06 22:29:35 +0800

    add signup feature

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
   db.sequelize.sync()
