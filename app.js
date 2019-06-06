0d2ff97ea8372eeb824a4515225a8e54160ba0bc
Tzuyi Yang <a0988426059@gmail.com>
2019-06-06 23:18:55 +0800

    Form validation and error messages

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(3000, () => {
   db.sequelize.sync()
