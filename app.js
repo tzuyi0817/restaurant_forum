c86a3a60a2ffc949b5135c8a50272d929b47fef1
Tzuyi Yang <a0988426059@gmail.com>
2019-06-07 14:06:40 +0800

    add signin and logout feature

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(session({
   secret: 'secret',
@@ -28,4 +31,4 @@ app.listen(3000, () => {
   console.log('Example app listening on http://localhost:3000')
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
