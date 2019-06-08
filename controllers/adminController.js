71ce2677922b332271c75af9942b462a74cae469
Tzuyi Yang <a0988426059@gmail.com>
2019-06-08 13:48:39 +0800

    add create restaurant feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,27 @@ const adminController = {
     return Restaurant.findAll().then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '未填寫餐廳名稱')
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    }).then(restaurant => {
+      req.flash('success_messages', '餐廳新增成功')
+      res.redirect('/admin/restaurants')
+    })
   }
 }
 
