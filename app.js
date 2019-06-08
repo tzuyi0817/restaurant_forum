f4576e1463da800aeca5638456506758d802c095
Tzuyi Yang <a0988426059@gmail.com>
2019-06-08 17:15:29 +0800

    add upload photos feature

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use(session({
   secret: 'secret',
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
+const fs = require('fs')
 
 const adminController = {
   getRestaurants: (req, res) => {
@@ -17,16 +18,38 @@ const adminController = {
       req.flash('error_messages', '未填寫餐廳名稱')
       return res.redirect('back')
     }
-    return Restaurant.create({
-      name: req.body.name,
-      tel: req.body.tel,
-      address: req.body.address,
-      opening_hours: req.body.opening_hours,
-      description: req.body.description
-    }).then(restaurant => {
-      req.flash('success_messages', '餐廳新增成功')
-      res.redirect('/admin/restaurants')
-    })
+
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: file ? `/upload/${file.originalname}` : null
+          }).then(restaurant => {
+            req.flash('success_messages', '餐廳新增成功')
+            res.redirect('/admin/restaurants')
+          })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null
+      }).then(restaurant => {
+        req.flash('success_messages', '餐廳新增成功')
+        res.redirect('/admin/restaurants')
+      })
+    }
   },
 
   getRestaurant: (req, res) => {
@@ -46,18 +69,42 @@ const adminController = {
       req.flash('error_messages', '未填寫餐廳名稱')
       return res.redirect('back')
     }
-    return Restaurant.findByPk(req.params.id).then(restaurant => {
-      restaurant.update({
-        name: req.body.name,
-        tel: req.body.tel,
-        address: req.body.address,
-        opening_hours: req.body.opening_hours,
-        description: req.body.description
-      }).then(restaurant => {
-        req.flash('success_messages', '餐廳修改成功')
-        res.redirect('/admin/restaurants')
+
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.findByPk(req.params.id).then(restaurant => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hours: req.body.opening_hours,
+              description: req.body.description,
+              image: file ? `/upload/${file.originalname}` : restaurant.image
+            }).then(restaurant => {
+              req.flash('success_messages', '餐廳新增成功')
+              res.redirect('/admin/restaurants')
+            })
+          })
+        })
       })
-    })
+    } else {
+      return Restaurant.findByPk(req.params.id).then(restaurant => {
+        restaurant.update({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: restaurant.image
+        }).then(restaurant => {
+          req.flash('success_messages', '餐廳修改成功')
+          res.redirect('/admin/restaurants')
+        })
+      })
+    }
   },
 
   deleteRestaurant: (req, res) => {
