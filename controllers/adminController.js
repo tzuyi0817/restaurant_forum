ea2d220dff5843997c69d096f063a92cb3350486
Tzuyi Yang <a0988426059@gmail.com>
2019-06-10 23:11:03 +0800

    Add categories selector on admin create and edit page and add CategoryId to admin postRestaurant and putRestaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,7 +13,9 @@ const adminController = {
   },
 
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll().then(categories => {
+      return res.render('admin/create', { categories })
+    })
   },
 
   postRestaurant: (req, res) => {
@@ -32,7 +34,8 @@ const adminController = {
           address: req.body.address,
           opening_hours: req.body.opening_hours,
           description: req.body.description,
-          image: file ? img.data.link : null
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
         }).then(restaurant => {
           req.flash('success_messages', '餐廳新增成功')
           res.redirect('/admin/restaurants')
@@ -45,7 +48,8 @@ const adminController = {
         address: req.body.address,
         opening_hours: req.body.opening_hours,
         description: req.body.description,
-        image: null
+        image: null,
+        CategoryId: req.body.categoryId
       }).then(restaurant => {
         req.flash('success_messages', '餐廳新增成功')
         res.redirect('/admin/restaurants')
@@ -61,7 +65,9 @@ const adminController = {
 
   editRestaurant: (req, res) => {
     Restaurant.findByPk(req.params.id).then(restaurant => {
-      res.render('admin/create', { restaurant })
+      Category.findAll().then(categories => {
+        res.render('admin/create', { restaurant, categories })
+      })
     })
   },
 
@@ -82,7 +88,8 @@ const adminController = {
             address: req.body.address,
             opening_hours: req.body.opening_hours,
             description: req.body.description,
-            image: file ? img.data.link : restaurant.image
+            image: file ? img.data.link : restaurant.image,
+            CategoryId: req.body.categoryId
           }).then(restaurant => {
             req.flash('success_messages', '餐廳新增成功')
             res.redirect('/admin/restaurants')
@@ -97,7 +104,8 @@ const adminController = {
           address: req.body.address,
           opening_hours: req.body.opening_hours,
           description: req.body.description,
-          image: restaurant.image
+          image: restaurant.image,
+          CategoryId: req.body.categoryId
         }).then(restaurant => {
           req.flash('success_messages', '餐廳修改成功')
           res.redirect('/admin/restaurants')
