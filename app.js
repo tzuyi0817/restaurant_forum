45df87027f0876320edac864b6b043214b3bafa8
Tzuyi Yang <a0988426059@gmail.com>
2019-06-10 23:49:18 +0800

    Add handlebar helper ifCond and update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
