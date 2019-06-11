d7cdbc436d06a49b500b932e92f1bd323f43bdd2
Tzuyi Yang <a0988426059@gmail.com>
2019-06-11 23:01:57 +0800

    add admin categories delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -37,6 +37,14 @@ let categoryController = {
         })
       })
     }
+  },
+
+  deleteCategory: (req, res) => {
+    Category.findByPk(req.params.id).then(category => {
+      category.destroy().then(category => {
+        res.redirect('/admin/categories')
+      })
+    })
   }
 }
 
