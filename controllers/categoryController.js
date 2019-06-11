f4d762bc85289fdb3e95d44eea665726dd46464c
Tzuyi Yang <a0988426059@gmail.com>
2019-06-11 22:52:09 +0800

    add admin categories edit feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,12 @@ const Category = db.Category
 let categoryController = {
   getCategories: (req, res) => {
     Category.findAll().then(categories => {
-      res.render('admin/categories', { categories })
+      if (req.params.id) {
+        Category.findByPk(req.params.id).then(category => {
+          res.render('admin/categories', { categories, category })
+        })
+      } else
+        res.render('admin/categories', { categories })
     })
   },
 
@@ -16,6 +21,20 @@ let categoryController = {
       Category.create({ name: req.body.name }).then(category => {
         req.flash('success_messages', '分類新增成功')
         res.redirect('/admin/categories')
+      })
+    }
+  },
+
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '未填寫分類名稱')
+      res.redirect('back')
+    } else {
+      Category.findByPk(req.params.id).then(category => {
+        category.update(req.body).then(category => {
+          req.flash('success_messages', '分類修改成功')
+          res.redirect('/admin/categories')
+        })
       })
     }
   }
