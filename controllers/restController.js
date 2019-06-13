5996ec52efc5befe23d6bd2c2168d799639ee601
Tzuyi Yang <a0988426059@gmail.com>
2019-06-13 22:41:19 +0800

    show related comments on restaurant page andCocnvert time ormat using moment

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -32,7 +34,12 @@ const restController = {
   },
 
   getRestaurant: (req, res) => {
-    Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+    Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
       res.render('restaurant', { restaurant })
     })
   }
